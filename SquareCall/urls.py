af4a583529d49e19834a531e708e2d9bdadc8575
DESKTOP-S45CL6I\Winston <winstonsummers@live.com>
2018-06-28 13:57:07 -0700

    first db migration, planning tables for second migration

--- SquareCall/urls.py
+++ SquareCall/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('Stevie/', include('Stevie.urls')),
+    path('', include('Stevie.urls')),
     path('admin/', admin.site.urls),
 ]
