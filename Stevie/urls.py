1e3433a20859c34ad471852bacd2504144dd1121
DESKTOP-S45CL6I\Winston <winstonsummers@live.com>
2018-06-28 00:36:24 -0700

    set all routes, planning db(mongo)

--- Stevie/urls.py
+++ Stevie/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('venues', views.venues, name='venues'),
+    path('calendar', views.calendar, name='calendar'),
+    path('newsletter', views.newsletter, name="newsletter"),
 ]
--- Stevie/views.py
+++ Stevie/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!, This is Stephe's site.")
+
+def venues(request):
+    return HttpResponse("This will be the venues page.")
+
+def calendar(request):
+    return HttpResponse("This will be the calendar page.")
+
+def newsletter(request):
+    return HttpResponse("This will be the newsletter page.")
