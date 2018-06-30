316600979eb8b2c7256a033b06bad65b3fa1a312
DESKTOP-S45CL6I\Winston <winstonsummers@live.com>
2018-06-30 14:14:03 -0700

    sucessful migrations

--- SquareCall/settings.py
+++ SquareCall/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Stevie.apps.StevieConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Stevie/models.py
+++ Stevie/models.py
@@ -4,19 +4,19 @@ from django.db import models
 
 class Calendar(models.Model):
     date_time = models.DateTimeField()
-    address = models.CharField()
-    venue = models.CharField()
+    address = models.CharField(max_length=100)
+    venue = models.CharField(max_length=100)
     description = models.TextField()
     flyer = models.ImageField(upload_to='flyers')
     event_site = models.URLField()
 
 class Post(models.Model):
     #general values
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     text = models.TextField()
-    page = models.CharField()
+    page = models.CharField(max_length=20)
     #location page values
-    address = models.CharField()
-    venue = models.CharField()
+    address = models.CharField(max_length=100)
+    venue = models.CharField(max_length=100)
     #newsletter page values
     letter = models.FileField(upload_to='newsletters')
