cfd39302092c549fbd5c816f8a6c9f89ba03a4c1
DESKTOP-S45CL6I\Winston <winstonsummers@live.com>
2018-06-30 13:49:25 -0700

    finished models, about to migrate

--- Stevie/models.py
+++ Stevie/models.py
@@ -3,4 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Calendar(models.Model):
-    
+    date_time = models.DateTimeField()
+    address = models.CharField()
+    venue = models.CharField()
+    description = models.TextField()
+    flyer = models.ImageField(upload_to='flyers')
+    event_site = models.URLField()
+
+class Post(models.Model):
+    #general values
+    title = models.CharField()
+    text = models.TextField()
+    page = models.CharField()
+    #location page values
+    address = models.CharField()
+    venue = models.CharField()
+    #newsletter page values
+    letter = models.FileField(upload_to='newsletters')
