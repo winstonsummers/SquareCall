abfd29a300be00993e942a3e6d6d3b6dba64b44d
DESKTOP-S45CL6I\Winston <winstonsummers@live.com>
2018-07-02 13:53:53 -0700

    Researched and started templating for project specific use case.

--- Stevie/views.py
+++ Stevie/views.py
@@ -5,20 +5,20 @@ from .models import Post
 # Create your views here.
 def index(request):
     post = Post.objects.filter(page='HM')
-    context = {'post' : post,}
+    context = {'post' : post, 'title': '',}
     return render(request, 'Stevie/index.html', context) 
 
 def venues(request):
     post = Post.objects.filter(page='VN')
-    context = {'post' : post,}
+    context = {'post' : post, 'title': '-- Venues',}
     return render(request, 'Stevie/index.html', context) 
 
 def calendar(request):
     post = Post.objects.filter(page='CL')
-    context = {'post' : post,}
+    context = {'post' : post, 'title': '-- Calendar',}
     return render(request, 'Stevie/index.html', context) 
 
 def newsletter(request):
     post = Post.objects.filter(page='NL')
-    context = {'post' : post,}
+    context = {'post' : post, 'title': '-- esSence Newsletter',}
     return render(request, 'Stevie/index.html', context) 
