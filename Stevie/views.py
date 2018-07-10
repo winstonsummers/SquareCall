46599884ac823b41a925419cb5ca01dcf9536108
DESKTOP-S45CL6I\Winston <winstonsummers@live.com>
2018-07-09 20:35:27 -0700

    sidebar func finished, basic grid(needs some fixes), possible background color tbd

--- Stevie/views.py
+++ Stevie/views.py
@@ -4,21 +4,29 @@ from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 def index(request):
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='HM')
-    context = {'post' : post, 'title': '',}
+    context = {'post' : post, 'title': '', 'side': side, 'info': info,}
     return render(request, 'Stevie/index.html', context) 
 
-def venues(request):
+def venues(request):    
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='VN')
-    context = {'post' : post, 'title': '-- Venues',}
+    context = {'post' : post, 'title': '-- Venues', 'side': side, 'info': info,}
     return render(request, 'Stevie/index.html', context) 
 
 def calendar(request):
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='CL')
-    context = {'post' : post, 'title': '-- Calendar',}
+    context = {'post' : post, 'title': '-- Calendar', 'side': side, 'info': info,}
     return render(request, 'Stevie/index.html', context) 
 
 def newsletter(request):
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='NL')
-    context = {'post' : post, 'title': '-- esSence Newsletter',}
+    context = {'post' : post, 'title': '-- esSence Newsletter', 'side': side, 'info': info,}
     return render(request, 'Stevie/index.html', context) 
