9c8867134a021cfdbfe1c9c570806462027ea207
Winston Summers <wsummers@zulily.com>
2018-09-12 17:52:37 -0700

    fixed instance bug and added page titles to html title

--- Stevie/views.py
+++ Stevie/views.py
@@ -4,31 +4,44 @@ import datetime
 
 from .models import Post, Calendar
 # Create your views here.
-cal = Calendar.objects.all()[:5]
-side = Post.objects.filter(page='SB')
-info = Post.objects.filter(page='CI')
+
 
 def index(request):
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='HM')
-    context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
+    context = {'post' : post, 'title': '- The Website', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context) 
 
-def venues(request):    
+def venues(request):   
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI') 
     post = Post.objects.filter(page='VN')
-    context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
+    context = {'post' : post, 'title': '- Where I Call', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context) 
 
 def calendar(request):
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='CL')
-    context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
+    context = {'post' : post, 'title': '- Calendar', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context) 
 
 def essense(request):
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='ES')
-    context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
+    context = {'post' : post, 'title': '- esSense', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context) 
 
 def newsnnotes(request):
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='NN')
-    context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
+    context = {'post' : post, 'title': '- NEWS\'n NOTES', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context)
