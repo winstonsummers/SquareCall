0dab98d5ed8c58c8929a67d90458bb20331e7179
Winston Summers <wsummers@zulily.com>
2018-09-22 20:09:32 -0700

    fixed views for merge

--- Stevie/views.py
+++ Stevie/views.py
@@ -6,24 +6,30 @@ from mailchimp3 import MailChimp
 
 from .models import Post, Calendar
 # Create your views here.
-cal = Calendar.objects.all()[:5]
-side = Post.objects.filter(page='SB')
-info = Post.objects.filter(page='CI')
 
 API_KEY = '0992de85e1ae5fcc1534597d2da7fc4d-us19'
 LIST_ID = 'f93459e3a2'
 
 def index(request):
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='HM')
     context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context) 
 
 def venues(request):    
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='VN')
     context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context) 
 
 def calendar(request):
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='CL')
     context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context) 
@@ -35,6 +41,9 @@ def calendar(request):
 
 def essense(request):
     if request.method == 'GET':
+        cal = Calendar.objects.all()[:5]
+        side = Post.objects.filter(page='SB')
+        info = Post.objects.filter(page='CI')
         post = Post.objects.filter(page='ES')
         context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
         #if get, show past newsletters
@@ -53,6 +62,9 @@ def essense(request):
         return redirect('essense')
 
 def newsnnotes(request):
+    cal = Calendar.objects.all()[:5]
+    side = Post.objects.filter(page='SB')
+    info = Post.objects.filter(page='CI')
     post = Post.objects.filter(page='NN')
     context = {'post' : post, 'title': '', 'side': side, 'info': info, 'cal': cal}
     return render(request, 'Stevie/index.html', context)
